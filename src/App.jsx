e40998af0fee7e0f0624da2d69da6bd9f69c32eb
Wai Ian Vong <valenvwi@gmail.com>
2023-08-09 10:58:53 +0200

    clean up and delete unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Table from './Table'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>To do list</h1>
+      <h1>🍎 To do list 🏃‍♀️</h1>
       <div className="card">
         <Table />
       </div>
